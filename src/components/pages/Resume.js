ef84a85b6b0438a3279f3d8cea77683761dcbe1d
Jon Kelly <jonfkelly@gmail.com>
2022-11-29 17:51:35 -0600

    started resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React from "react";
 
 export default function Resume() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+    <div className="border border-black border-solid mb-8">
+    <div className="border white-border font-light border-solid rounded-sm py-12 px-8  blkBackground">
+      <h1 className="text-4xl text-white mb-3 text-center">Resume</h1>
+      <p className="text-white text-2xl text-center red-text mt-6">Schooling</p>
+      <p className="text-white text-lg text-center">
+        Degree in Letters (yes, it's a real degree) from the University of Oklahoma
+        2011
       </p>
+      <p className="text-white text-lg text-center">
+        Full stack development bootcamp from Southern Methodist University 2022
+      </p>
+      <p className="text-white text-2xl mt-3 text-center blue-text">Skills</p>
+      <div class="grid grid-cols-2 text-white text-center text-lg px-72">
+      <p className="">
+        Full Stack development
+      </p>
+      <p className="">
+        HTML/CSS
+      </p>
+      <p className="">
+        JavaScript
+      </p>
+      <p className="">
+        React
+      </p>
+      <p className="">
+        SQL/NoSQL
+      </p>
+      <p className="">
+        GraphQL/Rest
+      </p>
+      </div>
+      <p className="text-white text-2xl mt-3 text-center yellow-text">Job History</p>
+      <p className="text-white text-lg text-center">
+        Degree in Letters (its a real degree) from the University of Oklahoma
+        2011
+      </p>
+      <p className="text-white text-lg text-center">
+        Full stack development bootcamp from Southern Methodist University 2022
+      </p>
+    </div>
     </div>
   );
 }
